86cf96d77cfc1214f4567b9cc97f6ac4f11322e9
nishantmg <nishan.blkx@gmail.com>
2020-02-09 05:58:24 +0545

    changes in app.js file

--- app.js
+++ app.js
@@ -26,6 +26,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
